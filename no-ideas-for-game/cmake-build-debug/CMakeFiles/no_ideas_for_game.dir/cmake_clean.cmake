39a3fd9ad0a44888d3fd62f1081a2ecdc351bc0b
Устьянцев Андрей <vydramain@gmail.com>
2019-07-10 10:17:06 +0600

    add readme and gitignore

---
+++ no-ideas-for-game/cmake-build-debug/CMakeFiles/no_ideas_for_game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/no_ideas_for_game.dir/main.cpp.o"
+  "no_ideas_for_game.pdb"
+  "no_ideas_for_game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/no_ideas_for_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
