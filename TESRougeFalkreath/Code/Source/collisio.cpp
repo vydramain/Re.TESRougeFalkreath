09e9257bfd94b0052dc1b698ca85b6a75ae67b6e
Andrey Ustyantsev <vydramain@gmail.com>
2019-07-17 09:22:26 +0600

    [+] В полисках коллизий

---
+++ TESRougeFalkreath/Code/Source/collisio.cpp
@@ -0,0 +1,22 @@
+//
+// Created by vydra on 7/17/19.
+//
+
+#include "collisio.h"
+
+
+bool collisio::isInCollisio(unsigned x, unsigned y) {
+  if ((x > x_ && x < x_s) && (y > y_ && y < y_s)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool collisio::isOutCollisio(unsigned x, unsigned y) {
+  if ((x > x_ && x < x_s) && (y > y_ && y < y_s)) {
+    return false;
+  } else {
+    return true;
+  }
+}
