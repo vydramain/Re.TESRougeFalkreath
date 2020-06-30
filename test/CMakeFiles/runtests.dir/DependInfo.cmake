6c868bfcd7992393c8f33593411e4ae9fc2c5830
Vydra <vydramain@gmail.com>
2020-07-01 03:20:28 +0600

    [+] added Makefile

---
+++ test/CMakeFiles/runtests.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/vydra/Documents/Some Gits/Coding Garden/ustjantsev-andrej-game/test/main.c" "/home/vydra/Documents/Some Gits/Coding Garden/ustjantsev-andrej-game/test/CMakeFiles/runtests.dir/main.c.o"
+  "/home/vydra/Documents/Some Gits/Coding Garden/ustjantsev-andrej-game/test/utils.c" "/home/vydra/Documents/Some Gits/Coding Garden/ustjantsev-andrej-game/test/CMakeFiles/runtests.dir/utils.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "lib/BearLibTerminal/include"
+  "src"
+  "../includes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ test/CMakeFiles/runtests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runtests.dir/main.c.o"
+  "CMakeFiles/runtests.dir/utils.c.o"
+  "runtests.pdb"
+  "runtests"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/runtests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
