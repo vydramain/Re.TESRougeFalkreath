fd0dec65012f4c51ff4a08943bcc16387375f517
Vydra <vydramain@gmail.com>
2020-07-01 03:09:49 +0600

    [*] BearLibTerminal shows phrase

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,14 @@
 #include "BearLibTerminal.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    
+    terminal_open();
+    terminal_set("window: title='re:TESFalkreath', cellsize=8x15, size=100x40, fullscreen=true");
+    terminal_refresh();
+    terminal_printf(0, 0, "Dras'kay Nirni!");    // Ta'agra
+    terminal_refresh();
+
+    while (terminal_read() != TK_CLOSE);
+
+    terminal_close();
     return 0;
 }
