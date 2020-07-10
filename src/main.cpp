55566441d21965bd73ca83082f51ba0d8716a6ab
Vydra <vydramain@gmail.com>
2020-07-11 02:06:18 +0600

    clean main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
 #include "BearLibTerminal.h"
+#include "gsm/interface_gsm.h"
+#include "gsm/game_states/interface_game_state.h"
+#include "gsm/game_states/global_game_states/global_game_state.h"
 
 int main() {
 
