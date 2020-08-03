ae87286dc5b3363f6b40069b3410f9054637f68e
Vydra <vydramain@gmail.com>
2020-08-03 06:16:02 +0600

    [*] lost Item.cpp

--- src/ecs/entities/items_entities/Item.cpp
+++ src/ecs/entities/items_entities/Item.cpp
@@ -7,6 +7,9 @@
 Item::Item(const char* input_name, bool input_pick, unsigned input_x, unsigned input_y)
     : Pickable(input_pick, input_x, input_y), name(input_name) {}
 
+Item::Item(const Item& input_item)
+    : Pickable(input_item.has_pick_up(), get_current_x(), get_current_y()), name(get_name()) {}
+
 Item::~Item() = default;
 
 void Item::set_name(const char* input_name) {
