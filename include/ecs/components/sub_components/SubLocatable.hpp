afa039840e24b16b290fbad66fda02cd31d68cdf
Vydra <vydramain@gmail.com>
2020-08-07 02:43:46 +0600

    [+] added abstract and sub Locatable component

---
+++ include/ecs/components/sub_components/SubLocatable.hpp
@@ -0,0 +1,19 @@
+//
+// Created by vydra on 8/7/20.
+//
+
+#pragma once
+
+#include "ecs/components/abstract_components/AbsLocatable.hpp"
+
+class SubLocatable : public AbsLocatable {
+public:
+    explicit SubLocatable(unsigned input_x, unsigned input_y);
+    ~SubLocatable();
+
+    unsigned get_current_x() const;
+    unsigned get_current_y() const;
+
+    void set_current_x(unsigned int input_x);
+    void set_current_y(unsigned int input_y);
+};
---
+++ src/ecs/components/sub_components/SubLocatable.cpp
@@ -0,0 +1,25 @@
+//
+// Created by vydra on 8/7/20.
+//
+
+#include "ecs/components/sub_components/SubLocatable.hpp"
+
+SubLocatable::SubLocatable(unsigned int input_x, unsigned int input_y) : AbsLocatable(input_x, input_y) {}
+
+SubLocatable::~SubLocatable() = default;
+
+unsigned int SubLocatable::get_current_x() const {
+    return current_x;
+}
+
+unsigned int SubLocatable::get_current_y() const {
+    return current_y;
+}
+
+void SubLocatable::set_current_x(unsigned int input_x) {
+    current_x = input_x;
+}
+
+void SubLocatable::set_current_y(unsigned int input_y) {
+    current_y = input_y;
+}
