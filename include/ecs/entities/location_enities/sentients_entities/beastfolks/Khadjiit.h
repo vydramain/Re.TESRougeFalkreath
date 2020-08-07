c718115a432cdc7d58c16ed834ec32c160d5eb5c
Vydra <vydramain@gmail.com>
2020-08-07 12:27:05 +0600

    [*]  changed header guard in Khadjiit

--- include/ecs/entities/location_enities/sentients_entities/beastfolks/Khadjiit.h
+++ include/ecs/entities/location_enities/sentients_entities/beastfolks/Khadjiit.h
@@ -2,8 +2,7 @@
 // Created by vydra on 8/3/20.
 //
 
-#ifndef INCLUDE_ECS_ENTITIES_RACES_ENTITIES_BEASTFOLK_RACE_KHADJIIT_H_
-#define INCLUDE_ECS_ENTITIES_RACES_ENTITIES_BEASTFOLK_RACE_KHADJIIT_H_
+#pragma once
 
 #include "ecs/entities/location_enities/sentients_entities/Sentient.h"
 
@@ -12,5 +11,3 @@ class Khadjiit : public Sentient {
   explicit Khadjiit(const char *input_name, unsigned input_current_x = 0, unsigned input_current_y = 0,
                     unsigned input_pocket = 10);
 };
-
-#endif  // INCLUDE_ECS_ENTITIES_RACES_ENTITIES_BEASTFOLK_RACE_KHADJIIT_H_
