ec7b4805a4db3994a5527cd0a6b46d702b37badc
Vydra <vydramain@gmail.com>
2020-08-14 18:15:51 +0600

    [+] added roadbrick

---
+++ src/core/entities/location_entities/ambient_entities/Roadbrick.cpp
@@ -0,0 +1,10 @@
+//
+// Created by vydra on 8/14/20.
+//
+
+#include "core/entities/location_enities/ambient_entities/Roadbrick.h"
+
+Roadbrick::Roadbrick(unsigned int input_x, unsigned int input_y)
+    : Ambient("Roadbrick", '.', 0x77ffffff, input_x, input_y) {}
+
+Roadbrick::~Roadbrick() = default;
