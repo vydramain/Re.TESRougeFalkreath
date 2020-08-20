8eed2d80336fc9c26d590996c9a6f62672a7cf89
Vydra <vydramain@gmail.com>
2020-08-20 14:20:16 +0600

    [*] fixed bakc move bug and added tips in score menu

--- include/systems/controls_systems/score_list_controls/SLControlSelectLeft.hpp
+++ include/systems/controls_systems/score_list_controls/SLControlSelectLeft.hpp
@@ -12,7 +12,7 @@ class SLControlSelectLeft : public IControl {
 
  public:
   explicit SLControlSelectLeft(ScoreListData *input_score_list_data) : IControl("SLControlSelectLeft") {
-    score_list_data = nullptr;
+    score_list_data = input_score_list_data;
   }
   ~SLControlSelectLeft() override {
     score_list_data = nullptr;
@@ -20,7 +20,7 @@ class SLControlSelectLeft : public IControl {
 
   void execute() override {
     if (score_list_data->get_current_page() > 0) {
-      score_list_data->set_current_page(score_list_data->get_current_page() + 1);
+      score_list_data->set_current_page(score_list_data->get_current_page() - 1);
     }
   }
 };
--- src/entities/render_entities/score_list_render/ScoreListRender.cpp
+++ src/entities/render_entities/score_list_render/ScoreListRender.cpp
@@ -21,6 +21,7 @@ void ScoreListRender::render() {
   terminal_color("White");
   const char *render_title[1] = {score_list_data->get_title()};
   TextPanelsRender::view_text(0, 0, SCREENMODE_X - 1, SCREENMODE_Y - 1, render_title, 1);
+  if (score_list_data->get_records_count() > 0) {
   const char **render_records;
   if (score_list_data->get_records_count() < 15) {
     render_records = new const char *[score_list_data->get_records_count()];
@@ -32,8 +33,17 @@ void ScoreListRender::render() {
     render_records[i] = score_list_data->get_record(i + score_list_data->get_current_page() * 15);
   }
   TextPanelsRender::view_text(0, 5, SCREENMODE_X - 1, SCREENMODE_Y - 1, render_records, i);
-  const char *render_message[1] = {"Нажмите 'Delete', чтобы отчистить"};
-  TextPanelsRender::view_text(SCREENMODE_X - 37, SCREENMODE_Y - 5, SCREENMODE_X - 1, SCREENMODE_Y - 1, render_message,
-                              1);
+
+    const char *render_message[1] = {"Нажмите 'Delete', чтобы отчистить"};
+    TextPanelsRender::view_text(SCREENMODE_X - 37, SCREENMODE_Y - 5, SCREENMODE_X - 1, SCREENMODE_Y - 1, render_message,
+                                1);
+    if (score_list_data->get_pages_count() > 0) {
+      const char *render_navigation[1] = {"Используйте ← →, чтобы перемещаться по таблице"};
+      TextPanelsRender::view_text(0, SCREENMODE_Y - 5, 50, SCREENMODE_Y - 1, render_navigation, 1);
+    }
+  } else {
+    const char *render_records[1] = {"Результаты отсутствуют"};
+    TextPanelsRender::view_text(0, 5, SCREENMODE_X - 1, SCREENMODE_Y - 1, render_records, 1);
+  }
   terminal_refresh();
 }
