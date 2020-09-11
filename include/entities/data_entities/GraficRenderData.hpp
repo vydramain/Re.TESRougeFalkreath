d62d3b26abaae09d10055780660f3f4693838d23
Vydra <vydramain@gmail.com>
2020-09-11 14:39:32 +0600

    [*] corrected render data

--- include/entities/data_entities/GraficRenderData.hpp
+++ include/entities/data_entities/GraficRenderData.hpp
@@ -6,17 +6,14 @@
 
 class GraficRenderData {
  private:
-  const char* tile_address;
   unsigned tile_number;
 
  public:
-  explicit GraficRenderData(const char *input_tile_address, unsigned input_tile_number);
+  explicit GraficRenderData(unsigned input_tile_number);
   ~GraficRenderData();
 
-  void set_tile_address(const char *input_tile_address);
   void set_tile_number(unsigned input_tile_number);
 
-  const char* get_tile_address() const;
   unsigned get_tile_number() const;
 
 };
