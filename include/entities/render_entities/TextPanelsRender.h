1c5a116d3eb4b2a53f68760cb41071575b476d47
Vydra <vydramain@gmail.com>
2020-09-17 09:10:51 +0600

    [*] added view input text method

--- include/entities/render_entities/TextPanelsRender.h
+++ include/entities/render_entities/TextPanelsRender.h
@@ -10,9 +10,14 @@
 #include "./BearLibTerminal.h"
 
 class TextPanelsRender {
+ private:
+  static void print_borders(unsigned in_x, unsigned in_y, unsigned out_x, unsigned out_y);
+
  public:
   static void view_strings_list(unsigned in_x, unsigned in_y, unsigned out_x, unsigned out_y,
                                 std::vector<std::string *> *input_strings);
   static void view_parameter_query(unsigned in_x, unsigned in_y, unsigned out_x, unsigned out_y, std::string *question,
                                    std::vector<std::string *> *input_choices, unsigned highlighted_choice);
+  static std::string *view_input_text(unsigned in_x, unsigned in_y, unsigned out_x, unsigned out_y, std::string *title,
+                                      unsigned input_max_count_characters);
 };
--- src/entities/render_entities/TextPanelsRender.cpp
+++ src/entities/render_entities/TextPanelsRender.cpp
@@ -7,8 +7,7 @@
 #include <string>
 #include <vector>
 
-void TextPanelsRender::view_strings_list(unsigned int in_x, unsigned int in_y, unsigned int out_x, unsigned int out_y,
-                                         std::vector<std::string *> *input_strings) {
+void TextPanelsRender::print_borders(unsigned int in_x, unsigned int in_y, unsigned int out_x, unsigned int out_y) {
   // cleaning up Area for message
   for (unsigned j = 0; j < out_y - in_y; j++) {
     for (unsigned i = 0; i < out_x - in_x; i++) {
@@ -29,6 +28,11 @@ void TextPanelsRender::view_strings_list(unsigned int in_x, unsigned int in_y, u
     terminal_print(in_x + 1 + i, in_y, "─");
     terminal_print(in_x + 1 + i, out_y, "─");
   }
+}
+
+void TextPanelsRender::view_strings_list(unsigned int in_x, unsigned int in_y, unsigned int out_x, unsigned int out_y,
+                                         std::vector<std::string *> *input_strings) {
+  print_borders(in_x, in_y, out_x, out_y);
 
   for (unsigned i = 0; i < input_strings->size(); i++) {
     terminal_print(in_x + 2, in_y + 2 + i, input_strings->at(i)->data());
@@ -38,26 +42,7 @@ void TextPanelsRender::view_strings_list(unsigned int in_x, unsigned int in_y, u
 void TextPanelsRender::view_parameter_query(unsigned in_x, unsigned int in_y, unsigned int out_x, unsigned int out_y,
                                             std::string *question, std::vector<std::string *> *input_choices,
                                             unsigned int highlighted_choice) {
-  // cleaning up area
-  for (unsigned ii(0); ii < (out_y - in_y); ii++) {
-    for (unsigned i(0); i < (out_x - in_x); i++) {
-      terminal_put(in_x + i, in_y + ii, ' ');
-    }
-  }
-
-  // draw borders
-  terminal_print(in_x, in_y, "┌");
-  terminal_print(out_x, in_y, "┐");
-  terminal_print(in_x, out_y, "└");
-  terminal_print(out_x, out_y, "┘");
-  for (unsigned j = 0; j < out_y - in_y - 1; j++) {
-    terminal_print(in_x, in_y + 1 + j, "│");
-    terminal_print(out_x, in_y + 1 + j, "│");
-  }
-  for (unsigned i = 0; i < out_x - in_x - 1; i++) {
-    terminal_print(in_x + 1 + i, in_y, "─");
-    terminal_print(in_x + 1 + i, out_y, "─");
-  }
+  print_borders(in_x, in_y, out_x, out_y);
 
   // draw question
   terminal_print(in_x + 7, in_y + 2, question->data());
@@ -67,4 +52,19 @@ void TextPanelsRender::view_parameter_query(unsigned in_x, unsigned int in_y, un
     }
     terminal_print(in_x + 7, in_y + 3 + i, input_choices->at(i - 1)->data());
   }
+}
+
+std::string *TextPanelsRender::view_input_text(unsigned int in_x, unsigned int in_y, unsigned int out_x,
+                                               unsigned int out_y, std::string *title,
+                                               unsigned int input_max_count_characters) {
+  char* temp_characters = new char[input_max_count_characters];
+  print_borders(in_x, in_y, out_x, out_y);
+
+  // draw question
+  terminal_print(in_x + 7, in_y + 2, title->data());
+  terminal_read_str(in_x + 7, in_y + 2, temp_characters, input_max_count_characters);
+
+  auto temp_str = new std::string(temp_characters);
+  delete [] temp_characters;
+  return temp_str;
 }
